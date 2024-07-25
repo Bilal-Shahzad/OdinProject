16e6557b02b03061af21f1a9a98bf6633c14490b
Bilal-Shahzad <b.shahzad01@gmail.com>
2024-07-24 22:18:16 -0500

    restaraun project

---
+++ JavaScript/Restaurant/index.js
@@ -0,0 +1,15 @@
+console.log('Webpack setup complete');
+
+function loadHome() {
+    const content = document.getElementById('content');
+    const homeDiv = document.createElement('div');
+    homeDiv.innerHTML = `
+      <h1>Welcome to Our Restaurant</h1>
+      <p>Enjoy our delicious food!</p>
+      <img src="path_to_image" alt="Restaurant Image">
+    `;
+    content.appendChild(homeDiv);
+  }
+  
+  loadHome();
+  
